26a905b8cd260ac72cb6d450c38eadc3eda7f3ed
Andreas Zhang <denglitsch@gmail.com>
2022-03-28 17:03:40 +0800

    update: data transfe ok

--- uart_byte_tx.v
+++ uart_byte_tx.v
@@ -3,9 +3,8 @@
 
 module uart_byte_tx #
 (
-    parameter       CLK_FREQ   = 'd50000000, // 50MHz
-    parameter       BAUD_RATE  = 'd9600,
-    parameter       SLICE_MODE = 1'b0
+    parameter       CLK_FREQ   = 'd50000000,// 50MHz
+    parameter       BAUD_RATE  = 'd9600    //
 )
 (
     input           clk_in,			// system clock
@@ -24,6 +23,7 @@ localparam START_BIT = 1'b0;
 localparam STOP_BIT = 1'b1;
 localparam BPS_PARAM = CLK_FREQ/BAUD_RATE;
 
+//start 8bit_data transfer operation
 reg tx_start_r;
 wire tx_start_pos;
 always@(posedge clk_in or negedge rst_n_in)
@@ -51,7 +51,7 @@ begin
         begin
 	        tx_state <= 1'b1;
         end
-        else if(tx_done)        // TODO: fixme
+        else if(tx_done)
         begin
 	        tx_state <= 1'b0;
         end
@@ -99,7 +99,7 @@ begin
     end
 	else
     begin
-        if(baud_rate_cnt == (SLICE_MODE ? (BPS_PARAM>>1) : 16'd1) )  // 0.5T or 0T
+        if(baud_rate_cnt == 16'd1 )
         begin
 		    bps_clk <= 1'b1;	
         end
@@ -150,5 +150,52 @@ begin
 	    tx_done <= 1'b0;
     end
 end
+
+reg [7:0] tx_data_r;
+always@(posedge clk_in or negedge rst_n_in)
+begin
+    if(!rst_n_in)
+    begin
+	    tx_data_r <= 8'b0;
+    end
+    else
+    begin
+        if(tx_start_pos)
+        begin
+	        tx_data_r <= tx_data;
+        end
+        else
+        begin
+	        tx_data_r <= tx_data_r;
+        end
+    end
+end
+
+always@(posedge clk_in or negedge rst_n_in)
+begin
+    if(!rst_n_in)
+    begin
+	    rs232_tx <= 1'b1;
+    end
+    else begin
+	    case(bps_cnt)
+		    0:rs232_tx  <= 1'b1;            // idle hi
+
+		    1:rs232_tx  <= START_BIT;       // start bit lo
+		    2:rs232_tx  <= tx_data_r[0];    // LSB first
+		    3:rs232_tx  <= tx_data_r[1];    //
+		    4:rs232_tx  <= tx_data_r[2];    //
+		    5:rs232_tx  <= tx_data_r[3];    //
+		    6:rs232_tx  <= tx_data_r[4];    //
+		    7:rs232_tx  <= tx_data_r[5];    //
+		    8:rs232_tx  <= tx_data_r[6];    //
+		    9:rs232_tx  <= tx_data_r[7];    // MSB last
+                                            // No parity
+		    10:rs232_tx <= STOP_BIT;        // stop bit hi
+
+		    default:rs232_tx <= 1'b1;       // idle hi
+	    endcase
+    end
+end	
 
 endmodule
