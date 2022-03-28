ebbd22221dfb086377950091b27d2e5d58ed8160
Andreas Zhang <denglitsch@gmail.com>
2022-03-28 22:36:06 +0800

    update: add rx bps clk

--- uart_byte_rx.v
+++ uart_byte_rx.v
@@ -13,10 +13,10 @@ module uart_byte_rx #
     output  reg [7:0]   rx_data,		// data need to transfer
     output  reg         rx_state,       // recieving duration
     output	reg         rx_done,        // pos-pulse for 1 tick indicates 1 byte transfer done
-    output	reg		    rs232_rx		// uart transfer pin
+    input		        rs232_rx		// uart transfer pin
 );
 
-localparam BPS_PARAM = CLK_FREQ/BAUD_RATE;
+localparam BPS_PARAM = (CLK_FREQ/BAUD_RATE)>>4; // oversample by x16
 
 reg	rs232_rx0,rs232_rx1,rs232_rx2;	
 //Detect negedge of rs232_rx
@@ -57,5 +57,96 @@ begin
     end
 end
 
+reg [15:0]  baud_rate_cnt;
+always@(posedge clk_in or negedge rst_n_in)
+begin
+    if(!rst_n_in)
+    begin
+        baud_rate_cnt <= 16'd0;
+    end
+    else
+    begin
+        if(rx_state)
+        begin
+            if(baud_rate_cnt >= BPS_PARAM - 1)
+            begin
+                baud_rate_cnt <= 16'd0;
+            end
+            else
+            begin
+                baud_rate_cnt <= baud_rate_cnt + 1'b1;
+            end
+        end
+        else
+        begin
+            baud_rate_cnt <= 16'd0;
+        end
+    end
+end
+    
+// generate bps_clk signal
+reg bps_clk;
+always @ (posedge clk_in or negedge rst_n_in)
+begin
+	if(!rst_n_in) 
+    begin
+		bps_clk <= `UD 1'b0;
+    end
+	else
+    begin
+        if(baud_rate_cnt == 16'd1 )
+        begin
+		    bps_clk <= `UD 1'b1;	
+        end
+	    else 
+        begin
+		    bps_clk <= `UD 1'b0;
+        end
+    end
+end
+
+//bps counter
+reg [7:0] bps_cnt;
+always@(posedge clk_in or negedge rst_n_in)
+begin
+    if(!rst_n_in)	
+    begin
+	    bps_cnt <= `UD 8'd0;
+    end
+    else
+    begin
+        if(bps_cnt>=8'd160-8'd1)        // TODO: fixme
+        begin
+	        bps_cnt <= `UD 4'd0;
+        end
+        else if(bps_clk)
+        begin
+	        bps_cnt <= `UD bps_cnt + 1'b1;
+        end
+        else
+        begin
+	        bps_cnt <= `UD bps_cnt;
+        end
+    end
+end
+
+always@(posedge clk_in or negedge rst_n_in)
+begin
+	if(!rst_n_in)
+    begin
+		rx_done <= 1'b0;
+    end
+	else
+    begin
+        if(bps_cnt == 8'd159)
+        begin
+		    rx_done <= 1'b1;
+        end
+	    else
+        begin
+		    rx_done <= 1'b0;
+        end
+    end
+end
 
 endmodule
